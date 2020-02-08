e86911dce5421342c8f4bcb2dd02d8e6ad9c8c3f
Paul Addonizio <pauladdonizio@gmail.com>
2020-02-08 15:05:11 -0600

    Basic TS Web Socket support

--- thavalon/typescript/communication.ts
+++ thavalon/typescript/communication.ts
@@ -1,3 +1,41 @@
 namespace ThavalonWeb.Communication {
-    export class WebSocketManager {}
+    export class WebSocketManager {
+        private readonly _webSocket: WebSocket;
+        //#region constructors
+        constructor()
+        constructor(webSocketUrl: string)
+        constructor(webSocketUrl?: string) {
+            // If there isn't a url, try to pull it from the window location.
+            if (typeof webSocketUrl === "undefined") {
+                let wsUrlPath = window.location.pathname.split("/");
+                wsUrlPath[wsUrlPath.length - 1] = "game";
+                webSocketUrl = "ws://" + window.location.host + "/" +
+                wsUrlPath.join("/");
+            }
+
+            // Now there should be a URL. Try to open a connnection.
+            this._webSocket = new WebSocket(webSocketUrl);
+            if (this._webSocket.readyState !== WebSocket.OPEN) {
+                throw new Error("The connection could not be established.");
+            }
+
+            // Connection opened successfully. Set up the required functions.
+            this._webSocket.onmessage = this.parseIncomingMessage;
+            this._webSocket.onerror = this.errorMessageHandler;
+        }
+        //#endregion
+
+        //#region public methods
+
+        //#endregion
+        //#region private methods
+        private parseIncomingMessage(rawMessage: MessageEvent): void {
+
+        }
+
+        private errorMessageHandler(rawMessage: MessageEvent): void {
+
+        }
+        //#endregion
+    }
 }
