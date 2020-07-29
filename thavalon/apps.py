15efe01dfa24f8659644c79315c7b6f42e792e73
theadd336 <pauladdonizio@gmail.com>
2020-07-28 23:49:39 -0500

    New Rust/React master

--- thavalon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThavalonConfig(AppConfig):
-    name = 'thavalon'
--- thavalon/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path("ws/thavalon/Lobby1/<slug:room_name>/", consumers.ChatConsumer),
-    path("ws/thavalon/<slug:game_id>/game/", consumers.GameConsumer),
-    path("ws/thavalon/<slug:game_id>/", consumers.LobbyConsumer),
-]
