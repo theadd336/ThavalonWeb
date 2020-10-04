4e8b2609965147d0ff7202c1a73fa280757f3be4
Paul <pauladdonizio@gmail.com>
2020-10-03 21:38:16 -0400

    Fix my stupidity

--- thavalon-webapp/src/App.tsx
+++ thavalon-webapp/src/App.tsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-=======
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Logout from './components/Logout';
->>>>>>> 93892a862d0c75c1bacc6fe00c685cc5e1fe369a
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -24,7 +18,7 @@ function App() {
           <h1>Home</h1>
         </Route>
         <Route path="/rules">
-          <h1>Rules</h1> 
+          <h1>Rules</h1>
         </Route>
         <Route path="/login">
           <Login setLoggedIn={() => setLoggedIn(true)} />
