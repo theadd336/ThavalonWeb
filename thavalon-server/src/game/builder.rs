7b8dfdb018b756cda533339b204f8f7e33b81ee1
Ben Navetta <ben.navetta@gmail.com>
2020-10-25 21:12:37 -0700

    Initial game implementation (#37)
    * logging
    
    * Handle mission-1 weirdness
    
    * cleanup to handle mission 1
    
    * probably-needless refactoring (tests broken)
    
    * rustfmt
    
    * implement game as direct async code
    
    * some more testing
    
    * rustfmt and maaaaybe Agravaine
    
    * cleanup, API for starting a game
    
    * cleanup
    
    * test Agravaine
    
    * feedback
    
    * feedback

---
+++ thavalon-server/src/game/builder.rs
@@ -0,0 +1,43 @@
+//! Builder for configuring and launching a new game.
+
+use tokio::sync::mpsc;
+use tokio::task;
+
+use super::engine;
+use super::interactions::ChannelInteractions;
+use super::messages::{Action, Message};
+use super::{Game, PlayerId};
+
+/// Builder for starting a new THavalon game
+pub struct GameBuilder {
+    interactions: ChannelInteractions,
+    players: Vec<(PlayerId, String)>,
+}
+
+impl GameBuilder {
+    /// Add a new player to the game. Any actions performed by the player should be sent to the returned `mpsc::Sender`. All messages
+    /// on the returned `mpsc::Receiver` should be shown to the player.
+    pub fn add_player(&mut self, name: String) -> (mpsc::Sender<Action>, mpsc::Receiver<Message>) {
+        let id = self.players.len() + 1;
+        // Allow a 10-message backlog for each channel, in case tasks get backed up.
+        let (action_tx, action_rx) = mpsc::channel(10);
+        let (message_tx, message_rx) = mpsc::channel(10);
+
+        self.interactions.add_player(id, action_rx, message_tx);
+        self.players.push((id, name));
+
+        (action_tx, message_rx)
+    }
+
+    /// Start the game. This consumes `self` because no new players can be added once the game starts.
+    /// The returned `task::JoinHandle` will complete once the game has ended.
+    pub fn start(self) -> task::JoinHandle<()> {
+        let game = Game::roll(self.players);
+        let mut interactions = self.interactions;
+        task::spawn(async move {
+            if let Err(e) = engine::run_game(&game, &mut interactions).await {
+                log::error!("Fatal game error: {}", e);
+            }
+        })
+    }
+}
