73569d5b50b3d6d0da6477c4c20f4d6b65027fe9
jtc2 <jaredjtc@gmail.com>
2020-10-25 20:30:21 -0400

    Navbar + Account Management - register, login, logout frontend (#35)
    * Update register page, create accountManager singleton class, navbar update on login
    
    * Comments, do not register if passwords don't match
    
    * login/out working, refresh token autocheck in progress
    
    * add typing to one place
    
    * Add typescript to github workflows yml, rename file
    
    * Fix indent in github workflows yml
    
    * Add tsc to github workflows yml
    
    * Update github workflows yml
    
    * Update yml again
    
    * PR comments, error updating, min password len 8
    
    * PR Reviews
    
    * In progress push to switch computers
    
    * PR comments + refresh token change
    
    * Switch to resolver for validating passwords match, per PR
    
    * Remove typescript workflow for now
    
    * PR Comments, start of account page will be continued in next PR
    
    * Fix account page location

--- thavalon-webapp/src/App.tsx
+++ thavalon-webapp/src/App.tsx
@@ -1,36 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from './components/Navbar';
+import { Navbar } from './components/Navbar';
 import { Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Register from './components/Register';
-import { say_hello } from './utils/account_utils';
+import { Login } from './components/Login';
+import { Logout } from './components/Logout';
+import { Register } from './components/Register';
+import { Home } from './components/Home';
+import { AccountManager, HttpResponse } from './utils/AccountManager';
+import { Account } from './components/Account';
+import ReactModal from 'react-modal';
+
+// Used by react modal for screen readers
+ReactModal.setAppElement("#root");
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(
-    () => setLoggedIn(loggedIn),
-    [loggedIn]
-  )
+  const [useMobileMenu, setUseMobileMenu] = useState(false);
+  // check logged in status within useEffect to not enter render loop
+  useEffect(() => {
+    const accountManager = AccountManager.getInstance();
+    accountManager.checkLoggedIn().then((httpResponse: HttpResponse) => {
+      // calling set logged in will, on success, trigger a timer to regularly check refresh token
+      setLoggedIn(httpResponse.result);
+    });  
+  })
+
+
   return (
     <div>
-      <Navbar loggedIn={loggedIn} />
+      <Navbar loggedIn={loggedIn} useMobileMenu={useMobileMenu} setUseMobileMenu={setUseMobileMenu} />
       <Switch>
         <Route path="/" exact>
-          <h1>Home</h1>
+          <Home />
         </Route>
         <Route path="/rules">
           <h1>Rules</h1>
-          {say_hello()}
         </Route>
-        <Route path="/login">
-          <Login setLoggedIn={() => setLoggedIn(true)} />
+        <Route path="/account">
+          <Account />
+        </Route>
+        <Route path="/login" render={
+          (_) => <Login setLoggedIn={setLoggedIn} />
+        }>
         </Route>
         <Route path="/logout">
           <Logout setLoggedIn={() => setLoggedIn(false)} />
         </Route>
-        <Route path="/register">
-          <Register />
+        <Route path="/register" render={
+          (_) => <Register setLoggedIn={setLoggedIn} />
+        }>
         </Route>
       </Switch>
     </div>
--- thavalon-webapp/src/components/Logout.tsx
+++ thavalon-webapp/src/components/Logout.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { log_out } from '../utils/account_utils';
+import { AccountManager, HttpResponse } from '../utils/AccountManager';
 
-type LogoutProps = {
-    setLoggedIn: any
+interface LogoutProps {
+    setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
 };
 
-function Logout(props: LogoutProps) {
-    useEffect(() => props.setLoggedIn(!log_out()));
+export function Logout(props: LogoutProps) {
+    useEffect(() => {
+        const accountManager = AccountManager.getInstance();
+        accountManager.logoutUser().then((httpResponse: HttpResponse) => {
+            if (httpResponse.result) {
+                props.setLoggedIn(false);
+            } else {
+                console.log("Failed to log out: " + httpResponse.message);
+            }
+        });
+    });
 
     return (
         <Redirect to="/" />
-    )
+    );
 }
-
-
-export default Logout;
