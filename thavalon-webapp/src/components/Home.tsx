8769681cbe1d6caa495d21ce11488a29169bd36e
Jared Cohen <jaredjtc@gmail.com>
2020-12-03 00:32:10 -0500

    remove test code

--- thavalon-webapp/src/components/Home.tsx
+++ thavalon-webapp/src/components/Home.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import { AccountManager, HttpResponse } from '../utils/AccountManager';
-import { GameSocket } from '../utils/GameSocket';
 
 export function Home(): JSX.Element {
-    const accountManager = AccountManager.getInstance();
-
-    function makeGame() {
-        accountManager.createGame().then((response: HttpResponse) => {
-            const name = "test";
-            const friendCode = response.message;
-            console.log(friendCode);
-            accountManager.joinGame(friendCode, name).then((response: HttpResponse) => {
-                console.log(response.message);
-                const gameSocket = new GameSocket(response.message);
-                console.log(gameSocket);
-                gameSocket.sendPing();
-            });
-        });
-    }
-
     return (
         <div>
             <h1>Hello World!</h1>
-            <button onClick={makeGame}>Make Game</button>
         </div>
     );
 }
