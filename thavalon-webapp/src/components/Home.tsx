31ee60db3a5ca11a3f89210c9c7793d3fdce375f
jtc2 <jaredjtc@gmail.com>
2020-12-08 23:22:06 -0500

    Start of Websocket + Create/Join Game functionality (#54)
    * Support rust integration tests
    
    * Fix YAML error
    
    * Update Readme
    
    * Use github generated badge
    
    * Change rust test name
    
    * Use playerID instead of email on the backend
    
    * Remove serde(default) from ThavalonUser
    
    * Validation.rs unit tests
    
    * Database crash hotfix
    
    * Fix remaining email references
    
    * Update version number
    
    * Samesite hotfix for Chrome users
    
    * Switch to TryFrom for InternalDBAccount conversion
    
    * Fix overzealous log::infos
    
    * Update register page, create accountManager singleton class, navbar update on login
    
    * Comments, do not register if passwords don't match
    
    * Initial support for unverified emails in the DB.
    
    * Add notifications module that doesn't work
    
    * Add notifications that don't work
    
    * login/out working, refresh token autocheck in progress
    
    * Working code paths for verification email.
    
    * add typing to one place
    
    * Add typescript to github workflows yml, rename file
    
    * Fix indent in github workflows yml
    
    * Add tsc to github workflows yml
    
    * Update github workflows yml
    
    * Update yml again
    
    * PR comments, error updating, min password len 8
    
    * PR Reviews
    
    * In progress push to switch computers
    
    * PR comments + refresh token change
    
    * Switch to resolver for validating passwords match, per PR
    
    * Remove typescript workflow for now
    
    * Working account verification
    
    * Module docs cleanup and additional error handling
    
    * PR Comments, start of account page will be continued in next PR
    
    * Fix account page location
    
    * Merging with master
    
    * Store rng locally and other minor fixes
    
    * New games modules for DB games
    
    * No idea what this is
    
    * Start modal rewrite
    
    * Start of rework of login modal
    
    * Update login page to use new input elements/buttons
    
    * Register modal rewrite
    
    * Restructure file directories
    
    * Style
    
    * Rename file to start with capital letter
    
    * Initial support for creating games
    
    * Add join game functionality to lobby
    
    * Lobby communication interfaces and join_game
    
    * Code for websockets. Currently doesn't compile
    
    * Barely working websockets
    
    * PR feedback
    
    * PR comments
    
    * add support for redirecting to prior non-modal page on modal close
    
    * register/login modal close support, have register page redirect to account page if already logged in
    
    * Add code documentation for game and lobby handlers
    
    * Updated logging and errors, but compilation problems
    
    * Working compiles and no errors
    
    * Ben's PR comments
    
    * Add Ping and Pong functionality
    
    * Switch to unique client ID for websocket connections
    
    * Start of websocket code, add create/join game code
    
    * PR feedback
    
    * remove test code
    
    * Switch gamesocket back to singleton
    
    * PR Feedback
    
    * PR Feedback
    
    * Remove quotes
    
    * PR Feedback
    
    Co-authored-by: Paul Addonizio <pauladdonizio@gmail.com>
    Co-authored-by: theadd336 <31495729+theadd336@users.noreply.github.com>

--- thavalon-webapp/src/components/Home.tsx
+++ thavalon-webapp/src/components/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export function Home(): JSX.Element {
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
     );
 }
